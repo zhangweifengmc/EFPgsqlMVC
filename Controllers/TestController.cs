3fe7bf06c4f185aac46e186cdb78dbffab805b32
zhangweifeng <282447146@qq.com>
2019-02-23 16:09:07 +0800

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using EFPgsqlMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFPgsqlMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            Database myDatabase = new Database();
+            //查询
+            var list = from s in myDatabase.adminuser
+                       select s;
+            return View(list);
+        }
+    }
+}
